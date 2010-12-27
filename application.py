be6b833cb98f0304dcef6bf4e7321cc98c1616ce
Gary Doran, Jr <garydoranjr@gmail.com>
2010-12-27 16:49:29 -0500

    Separate Facebook Post from Soupification

--- application.py
+++ application.py
@@ -9,28 +9,37 @@ template.register_template_library('common.my_filters')
 
 class MainPage(webapp.RequestHandler):
     def get(self):
-        self.response.out.write("""
-          <html>
-            <body>
-              <form action="/post" method="post">
-                <div><textarea name="message" rows="3" cols="60"></textarea></div>
-                <div><input type="submit" value="Soupify"></div>
-              </form>
-            </body>
-          </html>""")
-
+        path = static_page("index.html")
+        args = dict()
+        self.response.out.write(template.render(path, args))
 
 class FacebookPost(webapp.RequestHandler):
     def post(self):
-        msg = soupify(self.request.get('message'))
-        path = os.path.join(os.path.dirname(__file__), "post.html")
-        args = dict(soup=msg)
+        soupy = self.request.get('soup')
+        if not soupy:
+            soupy = soupify(self.request.get('message'))
+        path = static_page("post.html")
+        args = dict(soup=soupy)
+        self.response.out.write(template.render(path, args))
+
+class Soupify(webapp.RequestHandler):
+    def post(self):
+        msg = self.request.get('message')
+        soupy = soupify(msg)
+        path = static_page("soup.html")
+        args = dict(message=msg, soup=soupy)
         self.response.out.write(template.render(path, args))
 
 application = webapp.WSGIApplication(
                                      [('/', MainPage),
-                                      ('/post', FacebookPost)],
+                                      ('/post', FacebookPost),
+                                      ('/soup', Soupify)],
                                      debug=True)
+
+def static_page(suffix):
+    prefix = os.path.dirname(__file__)
+    path = os.path.join(prefix, suffix)
+    return path
 
 def main():
     from google.appengine.ext.webapp.util import run_wsgi_app
